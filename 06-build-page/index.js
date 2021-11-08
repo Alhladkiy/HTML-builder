b1d0c956e79285bc2b2bd1bf5955486338d3e539
Alhladkiy <Alhladkiy@gmail.com>
2021-11-08 22:31:47 +0300

    feat: add generate html file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,7 +8,7 @@ const newFolderPathAssets = path.join(__dirname, '/project-dist/assets');
 const newFilePathStyle = path.join(__dirname, '/project-dist/style.css');
 const newFilePathIndex = path.join(__dirname, '/project-dist/index.html');
 const folderPathAssets = path.join(__dirname, '/assets');
-
+const folderPathComponents = path.join(__dirname, '/components');
 
 const readStreamTemplate = fs.createReadStream(templatePath);
 
@@ -20,45 +20,61 @@ fs.mkdir(newFolderPath, {recursive: true}, (err) => {
     if (err) {
         throw err;
     } 
+
     fs.mkdir(newFolderPathAssets, {recursive: true}, (err) => {
         if (err) {
             throw err;
-        }      
+        }
+    });
+
     fs.writeFile(newFilePathStyle, '', (err) => {
         if (err) {
             throw err;
-        } 
+        }
+    });
+
     fs.writeFile(newFilePathIndex, '', (err) => {
         if (err) {
             throw err;
-        } 
+        }
+
         readStreamTemplate.on('data', (data) => {
-            console.log(data.toString());
-        }); 
-     
+            const regExp =  /{{([^]*?)}}/;
+            const promisesArray = [];
+            let template = data.toString();
+            let templateToUpdate = template;
+            let matchRes = template.match(regExp);
 
-        // });
-        // fs.readdir(folderPathAssets, {withFileTypes: true}, (err, files) => {
-        //     if (err) {
-        //         throw err;
-        //     }
-        //     for (let file of files) {
-        //         if (!file.isDirectory()) {
-        //             fs.copyFile(path.join(folderPathAssets, file.name), path.join(newFolderPathAssets, file.name), (err) => {
-        //                 if (err) {
-        //                     throw err;
-        //                 }
-        //                 console.log(file.name)
+            while (matchRes !== null) {
+                const tag = matchRes[1]
+                const textToReplace = matchRes[0];
+                const startIndex = matchRes.index;
 
-        //             });
-                    
-        //         }
-        //     }
-      
-        // })
-    })
-    })
-    })
+                templateToUpdate = templateToUpdate.slice(startIndex +  textToReplace.length);
+                matchRes = templateToUpdate.match(regExp);
 
+                const promise = new Promise((resolve, reject) => {
+                    const readStream = fs.createReadStream(path.join(folderPathComponents, `${tag}.html`), 'utf-8');
+                    readStream.on('data', (chunk) => {
+                        resolve({
+                            textToReplace,
+                            markup: chunk
+                        });
+                    });
+                });
 
-})
+                promisesArray.push(promise);
+            }
+            
+            Promise.all(promisesArray).then(res => {
+                res.forEach((x) => {
+                    template = template.replace(x.textToReplace, x.markup);
+                })
+
+                const writeStream = fs.createWriteStream(path.join(newFilePathIndex));
+
+                writeStream.write(template);
+            })
+        });     
+    });
+});
